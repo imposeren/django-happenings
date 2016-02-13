fab9edc3eb2138538531ea2a484a263872e8ae6a
Alex Recker <alex@reckerfamily.com>
2016-02-13 16:59:04 -0600

    Replaces deprecated patterns() with urlpatterns
    Replaces use of deprecated django urls API with newer implementation.

--- tests/urls.py
+++ tests/urls.py
@@ -1,17 +1,17 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '', url(
+urlpatterns = [
+    url(
         r'^calendar/', include('happenings.urls', namespace='calendar')
     ), url(
         r'^admin/', include(admin.site.urls)
     ),
-)
+]
 
 urlpatterns += staticfiles_urlpatterns()
